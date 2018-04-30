7c21780c28333d48fd12a256af3370a2e1cb97dc
Abhisek Das <abhisek@Abhiseks-MacBook-Pro.local>
2018-04-30 22:25:14 +0530

    MacCommit1025

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  posts: any[];
+  private url = 'http://jsonplaceholder.typicode.com/posts';
+  constructor(private http: Http) {
+    http.get(this.url)
+    .subscribe(response => {
+      this.posts = response.json() ;
+    });
   }
+  createPost(input: HTMLInputElement) {
+    // tslint:disable-next-line:prefer-const
+    let post = { title: input.value };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+        .subscribe(response => {
+          post['id'] = response.json().id;
+          this.posts.splice(0, 0, post);
+        });
+  }
+  updatePost(post) {
+    this.http.patch( this.url + '/' + post.id, JSON.stringify({ isRead: true}))
+        .subscribe(resposne => {
+            console.log(resposne.json());
+        });
+  }
+
 
 }
